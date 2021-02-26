889ac9095a57730b1190cd8d6dde5033c1b68285
Ludovic Rivallain <lrivallain@users.noreply.github.com>
2021-02-26 16:19:52 +0100

    Create startup-script.sh

---
+++ startup-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+apt update -qqy
+apt install -qqy python3-pip
+
+mkdir -p /opt/demo-app
+git clone https://github.com/lrivallain/demo-app.git /opt/demo-app
+
+echo "[Unit]
+After=network.service
+
+[Service]
+WorkingDirectory=/opt/demo-app
+ExecStart=bash /opt/demo-app/run.sh
+
+[Install]
+WantedBy=default.target" > /etc/systemd/system/demoapp.service
+chmod 644 /etc/systemd/system/demoapp.service
+
+systemctl daemon-reload
+systemctl enable demoapp.service
+systemctl start demoapp.service
+
+echo "The system is finally up, after $(uptime -p)"
